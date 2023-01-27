f76368ff50619e73d06171c6c99a1c1a0ca20fd6
Choiyoonji <cyj21c6352@gmail.com>
2023-01-27 19:31:59 +0900

    2178 solve

--- 백준/미로탐색_2178.py
+++ 백준/미로탐색_2178.py
@@ -1,22 +1,36 @@
 from collections import deque
 
-x = 0
-y = 0
 
-find = []
 ind = [[0,1],[1,0],[-1,0],[0,-1]]
 
 n, m = map(int,input().split())
 
 arr = [list(input()) for _ in range(n)]
 arr = [list(map(int,arr[i])) for i in range(n)]
-num = 0
 
-while True:
-    if x == n-1 and y == m-1:
-        break
+xy = deque([[0,0,1]])
+path = {}
+for i in range(n):
+    for j in range(m):
+        path[(i,j)] = 999999
+
     
-    find = [[x+ind[0][0],y+ind[0][1]]]
+while True:
+    p = xy.popleft()
+    x = p[0]
+    y = p[1]
+    
+    if x < 0 or y < 0 or x >= n or y >= m:
+        continue
+    
+    if arr[x][y] == 1:
+        arr[x][y] = -1
+        if path[(x,y)] > p[2]:
+            path[(x,y)] = p[2]
+        if x == n-1 and y == m-1:
+            break
+        for i in range(len(ind)):
+            xy.append([x+ind[i][0],y+ind[i][1],p[2]+1])
+            
 
-
-print(num)
+print(path[(n-1,m-1)])
