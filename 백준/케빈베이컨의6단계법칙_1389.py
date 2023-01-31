939ec75915e4b8c61c59973af672a7c6e2bc90db
Choiyoonji <cyj21c6352@gmail.com>
2023-01-31 21:02:22 +0900

    1389 solve

---
+++ 백준/케빈베이컨의6단계법칙_1389.py
@@ -0,0 +1,31 @@
+from math import inf
+[n,m] = list(map(int,input().split()))
+arr = [[-1 for i in range(n)] for j in range(n)]
+
+for i in range(m):
+    [a,b] = list(map(int, input().split()))
+    arr[a-1][b-1] = 1
+    arr[b-1][a-1] = 1
+
+
+for i in range(n):
+    for j in range(n):
+        for k in range(j,n):
+            if i == j:
+                break
+            if j == k:
+                arr[j][k] = 0
+            if arr[i][j] > 0 and arr[i][k] > 0:
+                if arr[j][k] == -1 or arr[j][k] > arr[i][j]+arr[i][k]:
+                    arr[j][k] = arr[i][j]+arr[i][k]
+                    arr[k][j] = arr[i][j]+arr[i][k]
+                
+minn = inf
+ans = 0
+for i in range(n):
+    su = sum(arr[i][:])
+    print(su)
+    if su < minn:
+        minn = su
+        ans = i
+print(ans)
